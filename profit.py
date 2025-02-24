646e82b2e2eb7ff821389e562c43a57fdff133aa
PlutoJoshua <thdude37@gmail.com>
2025-02-24 17:25:05 +0900

    해결 불가 ㅠㅠ

--- profit.py
+++ profit.py
@@ -154,14 +154,12 @@ def plot_matching_success(results_df, title):
     found_counts = results_df['found'].value_counts().reset_index()
     found_counts.columns = ['found', 'count']
     found_counts['found'] = found_counts['found'].map({True: 'Matched', False: 'Not Matched'})
-    found_counts = found_counts.to_pandas()
     fig = px.pie(
         found_counts, 
         values='count', 
         names='found', 
         title=title,
     )
-
 
     st.plotly_chart(fig)
 
